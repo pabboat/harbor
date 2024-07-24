87c70c0737b7646c52e3c41470eee9cadaf7f71d
pabboat <dylantrose@gmail.com>
2024-07-24 10:53:53 -0400

    first web scraper testing

---
+++ beatiful_soup/starter.py
@@ -0,0 +1,48 @@
+# Credit to tutorial: https://realpython.com/beautiful-soup-web-scraper-python/
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="ResultsContainer")
+
+def find_jobs(results):
+
+    job_elements = results.find_all("div", class_="card-content")
+
+    for job_element in job_elements:
+        title_element = job_element.find("h2", class_="title")
+        company_element = job_element.find("h3", class_="company")
+        location_element = job_element.find("p", class_="location")
+        print(title_element.text.strip())
+        print(company_element.text.strip())
+        print(location_element.text.strip())
+        print()
+
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+for job_element in python_job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    links = job_element.find_all("a")
+    link_url = job_element.find_all("a")[1]["href"]
+    print(f"Apply here: {link_url}\n")
+    print()
+
+
+if __name__ == "__main__":
+    pass
+    # find_jobs(results)
